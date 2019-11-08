1c52766bd05234844e0aeed10d1332772980b44e
cxin <13580112575@163.com>
2019-11-08 17:05:38 +0800

    cx-处理单个部门效能督查页面接口部门接件量11-8

--- aplanmis/src/main/resources/static/efficiency/js/singleAssEffect.js
+++ aplanmis/src/main/resources/static/efficiency/js/singleAssEffect.js
@@ -207,7 +207,7 @@ var app = new Vue({
       ts.pageSearchData.startTime = formatDate(new Date(ts.pageDateRange[0]), 'yyyy-MM-dd') || '';
       ts.pageSearchData.endTime = formatDate(new Date(ts.pageDateRange[1]), 'yyyy-MM-dd') || '';
       request('', {
-        url: ctx + 'org/efficiency/supervision/getOrgItemStatistics?',
+        url: ctx + 'org/efficiency/supervision/getOrgItemStatistics',
         type: 'get',
         data: ts.pageSearchData,
       }, function (res) {
