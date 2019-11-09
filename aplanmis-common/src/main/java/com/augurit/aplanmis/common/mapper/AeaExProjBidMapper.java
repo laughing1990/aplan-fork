e7c6db6d64f7ef81e80e4c0d9a6308b74dd93383
刘赵雄 <929404097@qq.com>
2019-11-09 16:41:15 +0800

    招投标信息

---
+++ aplanmis-common/src/main/java/com/augurit/aplanmis/common/mapper/AeaExProjBidMapper.java
@@ -0,0 +1,25 @@
+package com.augurit.aplanmis.common.mapper;
+
+import com.augurit.aplanmis.common.domain.AeaExProjBid;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+* 招投标信息-Mapper数据与持久化接口类
+<ul>
+    <li>项目名：奥格erp3.0--第一期建设项目</li>
+    <li>版本信息：v1.0</li>
+    <li>版权所有(C)2016广州奥格智能科技有限公司-版权所有</li>
+    <li>创建人:刘赵雄</li>
+    <li>创建时间：2019-10-31 15:56:12</li>
+</ul>
+*/
+@Mapper
+public interface AeaExProjBidMapper {
+
+public void insertAeaExProjBid(AeaExProjBid aeaExProjBid) throws Exception;
+
+public void updateAeaExProjBid(AeaExProjBid aeaExProjBid) throws Exception;
+
+public AeaExProjBid getAeaExProjBidByProjId(@Param("projId") String projId) throws Exception;
+}
---
+++ aplanmis/src/main/java/com/augurit/aplanmis/front/form/service/AeaExProjBidService.java
@@ -0,0 +1,23 @@
+package com.augurit.aplanmis.front.form.service;
+
+import com.augurit.aplanmis.common.domain.AeaExProjBid;
+
+/**
+* 招投标信息-Service服务调用接口类
+<ul>
+    <li>项目名：奥格erp3.0--第一期建设项目</li>
+    <li>版本信息：v1.0</li>
+    <li>版权所有(C)2016广州奥格智能科技有限公司-版权所有</li>
+    <li>创建人:刘赵雄</li>
+    <li>创建时间：2019-10-31 15:56:12</li>
+</ul>
+*/
+public interface AeaExProjBidService {
+
+    public void saveAeaExProjBid(AeaExProjBid aeaExProjBid) throws Exception;
+
+    public void updateAeaExProjBid(AeaExProjBid aeaExProjBid) throws Exception;
+
+    public AeaExProjBid getAeaExProjBidByProjId(String projId) throws Exception;
+
+}
