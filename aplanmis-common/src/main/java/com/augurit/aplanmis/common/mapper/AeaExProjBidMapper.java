2f8b8c3efd08ef37ced44fbad4e1d235e9994b8d
刘赵雄 <929404097@qq.com>
2019-11-12 20:03:44 +0800

    招投标信息表单优化

--- aplanmis-common/src/main/java/com/augurit/aplanmis/common/mapper/AeaExProjBidMapper.java
+++ aplanmis-common/src/main/java/com/augurit/aplanmis/common/mapper/AeaExProjBidMapper.java
@@ -1,8 +1,12 @@
 package com.augurit.aplanmis.common.mapper;
 
 import com.augurit.aplanmis.common.domain.AeaExProjBid;
+import com.augurit.aplanmis.common.domain.AeaUnitInfo;
+import com.augurit.aplanmis.common.domain.AeaUnitProj;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
 * 招投标信息-Mapper数据与持久化接口类
@@ -22,4 +26,11 @@ public void insertAeaExProjBid(AeaExProjBid aeaExProjBid) throws Exception;
 public void updateAeaExProjBid(AeaExProjBid aeaExProjBid) throws Exception;
 
 public AeaExProjBid getAeaExProjBidByProjId(@Param("projId") String projId) throws Exception;
+
+public List<AeaUnitInfo> findUnitProjByProjInfoIdAndType(@Param("projInfoId") String projInfoId, @Param("unitType") String unitType, @Param("rootOrgId") String rootOrgId);
+
+public List<AeaUnitProj> getAeaUnitProjById(@Param("projInfoId")String unitProjId);
+
+public int batchDeleteUnitProjByType(@Param("projId")String projId,@Param("unitTypes")List<String> unitTypes);
+
 }
