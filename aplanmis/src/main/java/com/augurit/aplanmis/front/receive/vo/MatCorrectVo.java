0916f98b8a03e95eb1c7d05828e9813f091e1200
xioahutu <1328669873@qq.com>
2019-11-12 14:46:46 +0800

    中介事项申报接口调试

--- aplanmis/src/main/java/com/augurit/aplanmis/front/receive/vo/MatCorrectVo.java
+++ aplanmis/src/main/java/com/augurit/aplanmis/front/receive/vo/MatCorrectVo.java
@@ -1,8 +1,10 @@
 package com.augurit.aplanmis.front.receive.vo;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class MatCorrectVo extends ReceiveBaseVo {
     private String chargeOrgName;
     private String receiveOrgName;
