ab153889917c98a86db13ad0c7e265c81bb219f8
xioahutu <1328669873@qq.com>
2019-11-12 14:39:30 +0800

    删除扫描漏洞

--- aplanmis/src/main/java/com/augurit/aplanmis/admin/item/controller/AeaItemOneformAdminController.java
+++ aplanmis/src/main/java/com/augurit/aplanmis/admin/item/controller/AeaItemOneformAdminController.java
@@ -11,11 +11,8 @@ import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-import sun.swing.StringUIClientPropertyKey;
 
 import java.util.UUID;
 
