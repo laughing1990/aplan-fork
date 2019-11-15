361f33bcedc51c5e211057015a13caba3459187b
@Apple_QJP <285688338@qq.com>
2019-11-15 15:14:47 +0800

    网厅证照库接口

---
+++ aplanmis-mall/src/main/java/com/augurit/aplanmis/mall/cert/controller/AeaCertController.java
@@ -0,0 +1,265 @@
+package com.augurit.aplanmis.mall.cert.controller;
+
+import com.augurit.aplanmis.integration.license.dto.LicenseAuthResDTO;
+import com.augurit.aplanmis.mall.cert.service.AeaCertService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author QinJianping
+ * @date 2019/11/15  13:58
+ * @desc
+ **/
+@Api(description = "网厅申报-->电子证照",tags={"电子证照"})
+@RestController
+@RequestMapping("/aea/cert")
+public class AeaCertController {
+
+    private static Logger logger = LoggerFactory.getLogger(AeaCertController.class);
+
+    @Autowired
+    private AeaCertService aeaCertService;
+//    @RequestMapping("/index.do")
+//    public ModelAndView index(ModelMap modelMap) {
+//
+//        getData(modelMap);
+//        return new ModelAndView("ui-jsp/cert/cert_index");
+//    }
+//
+//    /**
+//     * 获取数据字典数据
+//     *
+//     * @param modelMap
+//     */
+//    private void getData(ModelMap modelMap) {
+//
+//        OpuOmOrg topOrg = opuOmOrgService.getTopOrgByCurOrgId(SecurityContext.getCurrentOrgId());
+//        if (topOrg != null) {
+//            // 数据字典承诺办结时限单位
+//            List<BscDicCodeItem> dueUnits = bscDicCodeService.getActiveItemsByTypeCode("DUE_UNIT_TYPE", topOrg.getOrgId());
+//            modelMap.put("dueUnits", dueUnits);
+//
+//            // 数据字典获取持证所属类型
+//            List<BscDicCodeItem> certHolderTypes = bscDicCodeService.getActiveItemsByTypeCode("CERT_HOLDER_TYPE", topOrg.getOrgId());
+//            modelMap.put("certHolderTypes", certHolderTypes);
+//        }
+//    }
+//
+//    @RequestMapping("/listAeaCertByPage.do")
+//    public EasyuiPageInfo<AeaCert> listAeaCertByPage(AeaCert aeaCert, Page page) {
+//
+//        aeaCert.setRootOrgId(SecurityContext.getCurrentOrgId());
+//        logger.debug("分页查询，过滤条件为{}，查询关键字为{}", aeaCert);
+//        PageInfo<AeaCert> pageInfo = aeaCertService.listAeaCert(aeaCert, page);
+//        return PageHelper.toEasyuiPageInfo(pageInfo);
+//    }
+//
+//    @RequestMapping(value = "/gtreeBscAttDir.do", method = {RequestMethod.GET, RequestMethod.POST})
+//    public List<ZtreeNode> gtreeBscAttDir() throws Exception {
+//
+//        return aeaCertService.gtreeBscAttDir(SecurityContext.getCurrentOrgId());
+//    }
+//
+//    @RequestMapping("/gtreeTypeCert.do")
+//    public List<ZtreeNode> gtreeTypeCert() {
+//
+//        String rootOrgId = SecurityContext.getCurrentOrgId();
+//        return aeaCertService.gtreeTypeCert(rootOrgId);
+//    }
+//
+//    @ApiOperation(value = "获取证照最大排序号", notes = "获取证照最大排序号")
+//    @RequestMapping("/getMaxCertSortNo.do")
+//    public Long getMaxCertSortNo() {
+//
+//        String rootOrgId = SecurityContext.getCurrentOrgId();
+//        return aeaCertService.getMaxCertSortNo(rootOrgId);
+//    }
+//
+//    @ApiOperation(value = "检验证照是否存在")
+//    @ApiImplicitParams({
+//        @ApiImplicitParam(name = "certId", value = "证照id", type = "string"),
+//        @ApiImplicitParam(name = "certCode", value = "证照编号", type = "string")
+//    })
+//    @RequestMapping(value = "/checkUniqueCertCode.do", method = {RequestMethod.GET, RequestMethod.POST})
+//    public String checkUniqueCertCode(String certId, String certCode) {
+//
+//        if (StringUtils.isBlank(certCode)) {
+//            return "false";
+//        }
+//        String rootOrgId = SecurityContext.getCurrentOrgId();
+//        return aeaCertService.checkUniqueCertCode(certId, certCode, rootOrgId) + "";
+//    }
+//
+//    /**
+//     * 保存或编辑电子证照定义表
+//     *
+//     * @param aeaCert 电子证照定义表
+//     * @return 返回结果对象 包含结果信息
+//     */
+//    @ApiOperation(value = "保存或编辑电子证照", notes = "保存或编辑电子证照")
+//    @ApiImplicitParams({
+//        @ApiImplicitParam(name = "aeaCert", value = "非必填", dataType = "AeaCert" ,paramType = "body")
+//    })
+//    @RequestMapping(value = "/saveAeaCert.do", method = {RequestMethod.GET, RequestMethod.POST})
+//    public ResultForm saveAeaCert(AeaCert aeaCert) {
+//
+//        if (StringUtils.isNotBlank(aeaCert.getCertId())) {
+//            aeaCertService.updateAeaCert(aeaCert);
+//        } else {
+//            aeaCert.setCertId(UUID.randomUUID().toString());
+//            aeaCert.setRootOrgId(SecurityContext.getCurrentOrgId());
+//            aeaCertService.saveAeaCert(aeaCert);
+//        }
+//        return new ContentResultForm<>(true, aeaCert);
+//    }
+//
+//    @ApiOperation(value = "通过id删除电子证照数据", notes = "通过id删除电子证照数据")
+//    @ApiImplicitParams({
+//        @ApiImplicitParam(name = "id", value = "电子证照id", required = true , dataType = "String" ,paramType = "query"),
+//    })
+//    @RequestMapping(value = "/deleteAeaCertById.do", method = {RequestMethod.GET, RequestMethod.POST})
+//    public ResultForm deleteAeaCertById(String id) {
+//
+//        if (StringUtils.isNotBlank(id)) {
+//            logger.debug("删除电子证照定义表Form对象，对象id为：{}", id);
+//            aeaCertService.deleteAeaCertById(id);
+//            return new ResultForm(true);
+//        }
+//        return new ResultForm(false, "参数id为空!");
+//    }
+//
+//    @ApiOperation(value = "通过ids批量删除证照数据", notes = "通过ids批量删除证照数据")
+//    @ApiImplicitParams({
+//        @ApiImplicitParam(name = "ids", value = "证照id集合,以逗号分隔", required = true , dataType = "String" , allowMultiple = true, paramType = "query"),
+//    })
+//    @RequestMapping(value="/batchDeleteCertByIds.do", method = {RequestMethod.GET, RequestMethod.POST})
+//    public ResultForm batchDeleteCertByIds(String[] ids) {
+//
+//        if (ids != null && ids.length > 0) {
+//            logger.debug("删除电子证照定义表Form对象，对象id为：{}", ids);
+//            aeaCertService.batchDeleteCertByIds(ids);
+//            return new ResultForm(true);
+//        }
+//        return new ResultForm(false, "参数id为空!");
+//    }
+//
+//    @ApiOperation(value = "通过id获取电子证照数据", notes = "通过id获取电子证照数据")
+//    @ApiImplicitParams({
+//        @ApiImplicitParam(name = "id", value = "电子证照id", required = true , dataType = "String" ,paramType = "query"),
+//    })
+//    @RequestMapping("/getAeaCert.do")
+//    public AeaCert getAeaCert(String id) {
+//
+//        if (StringUtils.isNotBlank(id)) {
+//            logger.debug("根据ID获取AeaCert对象，ID为：{}", id);
+//            return aeaCertService.getAeaCertById(id);
+//        } else {
+//            logger.debug("构建新的AeaCert对象");
+//            return new AeaCert();
+//        }
+//    }
+//
+//    @RequestMapping("/listStageNoSelectCertByPage.do")
+//    public EasyuiPageInfo<AeaCert> listStageNoSelectCertByPage(AeaParIn in, Page page) {
+//
+//        PageInfo<AeaCert> pageInfo = aeaCertService.listStageNoSelectCertByPage(in, page);
+//        return PageHelper.toEasyuiPageInfo(pageInfo);
+//    }
+//
+//    @RequestMapping("/listStageNoSelectCert.do")
+//    public List<AeaCert> listStageNoSelectCert(AeaParIn in) {
+//
+//        return aeaCertService.listStageNoSelectCert(in);
+//    }
+//
+//    @RequestMapping("/listItemNoSelectCertByPage.do")
+//    public EasyuiPageInfo<AeaCert> listItemNoSelectCertByPage(AeaItemInout inout, Page page) {
+//
+//        PageInfo<AeaCert> pageInfo = aeaCertService.listItemNoSelectCertByPage(inout, page);
+//        return PageHelper.toEasyuiPageInfo(pageInfo);
+//    }
+//
+//    @RequestMapping("/listItemNoSelectCert.do")
+//    public List<AeaCert> listItemNoSelectCert(AeaItemInout inout) {
+//
+//        return aeaCertService.listItemNoSelectCert(inout);
+//    }
+//
+//    //===================  前后端分离新方法 ==================
+//
+//    @ApiOperation(value = "查询电子证照列表,带分页", notes = "查询电子证照列表,带分页")
+//    @ApiImplicitParams({
+//        @ApiImplicitParam(name = "cert", value = "非必填", dataType = "AeaCert" ,paramType = "body"),
+//        @ApiImplicitParam(name = "page", value = "分页信息",  dataType = "PageParam")
+//    })
+//    @RequestMapping(value = "/page.do", method = {RequestMethod.POST, RequestMethod.GET})
+//    public EasyuiPageInfo<AeaCert> listByPage(AeaCert cert, @ModelAttribute PageParam page){
+//
+//        logger.debug("分页查询，过滤条件为{}，查询关键字为{}", cert);
+//        PageInfo<AeaCert> pageInfo = aeaCertService.listAeaCert(cert, page.convertPage());
+//        return PageHelper.toEasyuiPageInfo(pageInfo);
+//    }
+//
+//    @ApiOperation(value = "检验证照是否存在", notes = "检验证照是否存在")
+//    @ApiImplicitParams({
+//        @ApiImplicitParam(name = "certId", value = "证照id", type = "string"),
+//        @ApiImplicitParam(name = "certCode", value = "证照编号", type = "string")
+//    })
+//    @RequestMapping(value = "/checkUniqueCode.do", method = {RequestMethod.GET, RequestMethod.POST})
+//    public Boolean checkUniqueCode(String certId, String certCode) {
+//
+//        if (StringUtils.isBlank(certCode)) {
+//            return false;
+//        }
+//        String rootOrgId = SecurityContext.getCurrentOrgId();
+//        return aeaCertService.checkUniqueCertCode(certId, certCode, rootOrgId);
+//    }
+//
+//    @ApiOperation(value = "获取文件库", notes = "获取文件库")
+//    @RequestMapping(value = "/gtreeAttDirForEui.do", method = {RequestMethod.GET, RequestMethod.POST})
+//    public List<ElementUiRsTreeNode> gtreeAttDirForEui() throws Exception {
+//
+//        return aeaCertService.gtreeAttDirForEui(SecurityContext.getCurrentOrgId());
+//    }
+//
+//    @ApiOperation(value = "获取承诺办结时限单位数据字典", notes = "获取承诺办结时限单位数据字典")
+//    @RequestMapping(value = "/listDueUnitType.do", method = {RequestMethod.GET, RequestMethod.POST})
+//    public List<BscDicCodeItem> listDueUnitType() throws Exception {
+//
+//        return bscDicCodeService.getActiveItemsByTypeCode("DUE_UNIT_TYPE", SecurityContext.getCurrentOrgId());
+//    }
+//
+//    @ApiOperation(value = "获取获取持证所属类型数据字典", notes = "获取获取持证所属类型数据字典")
+//    @RequestMapping(value = "/listCertHolderType.do", method = {RequestMethod.GET, RequestMethod.POST})
+//    public List<BscDicCodeItem> listCertHolderType() throws Exception {
+//
+//        return bscDicCodeService.getActiveItemsByTypeCode("CERT_HOLDER_TYPE", SecurityContext.getCurrentOrgId());
+//    }
+
+    @ApiOperation(value = "通过查询条件获取电子证照库数据", notes = "通过查询条件获取电子证照库数据")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "itemVerIds", value = "事项版本IDs", required = true , dataType = "String" ,paramType = "query"),
+            @ApiImplicitParam(name = "identityNumber", value = "申办证件号码", required = true , dataType = "String" ,paramType = "query"),
+    })
+    @RequestMapping("/getLicenseAuthRes.do")
+    public LicenseAuthResDTO getLicenseAuthRes(String itemVerIds, String identityNumber) throws Exception {
+        return aeaCertService.getLicenseAuthRes(itemVerIds, identityNumber);
+    }
+
+    @ApiOperation(value = "通过电子证照编码获取证照显示地址", notes = "通过电子证照编码获取证照显示地址")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "authCode", value = "证照编码", required = true , dataType = "String" ,paramType = "query"),
+    })
+    @RequestMapping("/getViewLicenseURL.do")
+    public String getViewLicenseURL(String authCode) throws Exception {
+        return aeaCertService.getViewLicenseURL(authCode);
+    }
+}
---
+++ aplanmis-mall/src/main/java/com/augurit/aplanmis/mall/cert/service/AeaCertService.java
@@ -0,0 +1,421 @@
+package com.augurit.aplanmis.mall.cert.service;
+
+import com.augurit.agcloud.bsc.domain.BscAttDir;
+import com.augurit.agcloud.bsc.domain.BscDicRegion;
+import com.augurit.agcloud.bsc.mapper.BscAttDirMapper;
+import com.augurit.agcloud.bsc.mapper.BscDicRegionMapper;
+import com.augurit.agcloud.framework.constant.Status;
+import com.augurit.agcloud.framework.exception.InvalidParameterException;
+import com.augurit.agcloud.framework.security.SecurityContext;
+import com.augurit.agcloud.framework.ui.elementui.ElementUiRsTreeNode;
+import com.augurit.agcloud.framework.ui.pager.PageHelper;
+import com.augurit.agcloud.framework.ui.ztree.ZtreeNode;
+import com.augurit.agcloud.framework.util.CollectionUtils;
+import com.augurit.agcloud.opus.common.domain.OpuOmOrg;
+import com.augurit.agcloud.opus.common.domain.OpuOmUserInfo;
+import com.augurit.agcloud.opus.common.mapper.OpuOmUserInfoMapper;
+import com.augurit.aplanmis.common.constants.DeletedStatus;
+import com.augurit.aplanmis.common.domain.*;
+import com.augurit.aplanmis.common.mapper.AeaCertMapper;
+import com.augurit.aplanmis.common.mapper.AeaCertTypeMapper;
+import com.augurit.aplanmis.common.mapper.AeaItemBasicMapper;
+import com.augurit.aplanmis.common.service.admin.opus.AplanmisOpuOmOrgAdminService;
+import com.augurit.aplanmis.common.service.instance.AeaHiIteminstService;
+import com.augurit.aplanmis.integration.license.config.LicenseConfig;
+import com.augurit.aplanmis.integration.license.dto.*;
+import com.augurit.aplanmis.integration.license.service.LicenseApiService;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageInfo;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author ZhangXinhui
+ * @date 2019/7/29 029 14:58
+ * @desc
+ **/
+@Service
+@Transactional
+public class AeaCertService {
+
+    private static Logger logger = LoggerFactory.getLogger(AeaCertService.class);
+
+    @Autowired
+    private AeaCertTypeMapper aeaCertTypeMapper;
+
+    @Autowired
+    private AeaCertMapper aeaCertMapper;
+
+    @Autowired
+    private BscAttDirMapper bscAttDirMapper;
+
+    @Autowired
+    private AeaHiIteminstService aeaHiIteminstService;
+
+    @Autowired
+    private AplanmisOpuOmOrgAdminService opuOmOrgService;
+    @Autowired
+    private BscDicRegionMapper bscDicRegionMapper;
+    @Autowired
+    private LicenseApiService licenseApiService;
+    @Autowired
+    private OpuOmUserInfoMapper opuOmUserInfoMapper;
+    @Autowired
+    private AeaItemBasicMapper aeaItemBasicMapper;
+    @Autowired
+    private LicenseConfig licenseConfig;
+
+    
+    public void deleteAeaCertById(String id) {
+
+        if (StringUtils.isNotBlank(id)) {
+            aeaCertMapper.deleteById(id);
+        }
+    }
+
+    
+    public void batchDeleteCertByIds(String[] ids) {
+
+        if (ids != null && ids.length > 0) {
+            aeaCertMapper.batchDeleteCertByCertIds(ids);
+        }
+    }
+
+    
+    public void saveAeaCert(AeaCert aeaCert) {
+
+        aeaCert.setRootOrgId(SecurityContext.getCurrentOrgId());
+        aeaCert.setCreater(SecurityContext.getCurrentUserId());
+        aeaCert.setCreateTime(LocalDateTime.now());
+        aeaCert.setIsDeleted(DeletedStatus.NOT_DELETED);
+        aeaCertMapper.insertOne(aeaCert);
+    }
+
+    
+    public void updateAeaCert(AeaCert aeaCert) {
+
+        aeaCert.setModifier(SecurityContext.getCurrentUserId());
+        aeaCert.setModifyTime(LocalDateTime.now());
+        aeaCertMapper.updateOne(aeaCert);
+    }
+
+
+    
+    public PageInfo<AeaCert> listAeaCert(AeaCert aeaCert, Page page) {
+
+        aeaCert.setRootOrgId(SecurityContext.getCurrentOrgId());
+        PageHelper.startPage(page);
+        List<AeaCert> list = aeaCertMapper.listAeaCert(aeaCert);
+        logger.debug("成功执行分页查询！");
+        return new PageInfo<>(list);
+    }
+
+    
+    public boolean checkUniqueCertCode(String certId, String certCode, String rootOrgId) {
+
+        Integer count = aeaCertMapper.checkUniqueCertCode(certId, certCode, rootOrgId);
+        return count == null || count <= 0;
+    }
+
+    
+    public List<ZtreeNode> gtreeBscAttDir(String orgId) throws Exception {
+
+        List<ZtreeNode> allNodes = new ArrayList<>();
+        if (StringUtils.isNotBlank(orgId)) {
+            List<BscAttDir> data = new ArrayList<>();
+            // 获取当前组织机构数据
+            BscAttDir dir = new BscAttDir();
+            dir.setOrgId(orgId);
+            dir.setIsRoot(Status.ON);
+            List<BscAttDir> list = bscAttDirMapper.listBscAttDir(dir);
+            if (list != null && list.size() > 0) {
+                data.addAll(list);
+            }
+            dir.setIsRoot(Status.OFF);
+            List<BscAttDir> list2 = bscAttDirMapper.listBscAttDir(dir);
+            if (CollectionUtils.isNotEmpty(list2)) {
+                data.addAll(list2);
+            }
+            if (CollectionUtils.isNotEmpty(data)) {
+                for (BscAttDir attDir : data) {
+                    ZtreeNode node = new ZtreeNode();
+                    node.setId(attDir.getDirId());
+                    node.setName(attDir.getDirName());
+                    node.setpId(attDir.getParentId());
+                    node.setOpen(true);
+                    node.setIsParent(true);
+                    if (StringUtils.isNotBlank(attDir.getParentId())) {
+                        node.setIsParent(false);
+                    }
+                    allNodes.add(node);
+                }
+            } else {
+                allNodes.add(buildZtreeNode());
+            }
+        } else {
+            allNodes.add(buildZtreeNode());
+        }
+        return allNodes;
+    }
+
+
+    public List<ElementUiRsTreeNode> gtreeAttDirForEui(String orgId) throws Exception {
+
+        List<ElementUiRsTreeNode> allNodes = new ArrayList<ElementUiRsTreeNode>();
+        ElementUiRsTreeNode rootNode = new ElementUiRsTreeNode();
+        rootNode.setId("root");
+        rootNode.setLabel("文件库");
+        rootNode.setType("root");
+        if (StringUtils.isNotBlank(orgId)) {
+            List<BscAttDir> data = new ArrayList<>();
+            // 获取当前组织机构数据
+            BscAttDir dir = new BscAttDir();
+            dir.setOrgId(orgId);
+            dir.setIsRoot(Status.ON);
+            List<BscAttDir> list = bscAttDirMapper.listBscAttDir(dir);
+            if (list != null && list.size() > 0) {
+                data.addAll(list);
+            }
+            dir.setIsRoot(Status.OFF);
+            List<BscAttDir> list2 = bscAttDirMapper.listBscAttDir(dir);
+            if (CollectionUtils.isNotEmpty(list2)) {
+                data.addAll(list2);
+            }
+            if (CollectionUtils.isNotEmpty(data)) {
+                for (BscAttDir attDir : data) {
+                    rootNode.getChildren().add(buildEUiNode(attDir));
+                }
+            }
+        }
+        allNodes.add(rootNode);
+        return allNodes;
+    }
+
+    private ElementUiRsTreeNode buildEUiNode(BscAttDir dir) {
+
+        ElementUiRsTreeNode node = new ElementUiRsTreeNode();
+        node.setId(dir.getDirId());
+        node.setLabel(dir.getDirName());
+        node.setType("cert");
+        node.setData(dir);
+        return node;
+    }
+
+    private ZtreeNode buildZtreeNode() {
+
+        ZtreeNode node = new ZtreeNode();
+        node.setId("root");
+        node.setName("文件库");
+        node.setOpen(true);
+        node.setIsParent(true);
+        node.setNocheck(true);
+        return node;
+    }
+
+    
+    public List<ZtreeNode> gtreeTypeCert(String rootOrgId) {
+
+        if (StringUtils.isBlank(rootOrgId)) {
+            rootOrgId = SecurityContext.getCurrentOrgId();
+        }
+        // 根节点
+        List<ZtreeNode> allNodes = new ArrayList<>();
+        ZtreeNode rootNode = new ZtreeNode();
+        rootNode.setId("root");
+        rootNode.setName("电子证照库");
+        rootNode.setType("root");
+        rootNode.setIsParent(true);
+        rootNode.setOpen(true);
+        rootNode.setNocheck(true);
+        allNodes.add(rootNode);
+        // 电子证照分类
+        AeaCertType scertType = new AeaCertType();
+        scertType.setRootOrgId(rootOrgId);
+        List<AeaCertType> certTypes = aeaCertTypeMapper.listAeaCertType(scertType);
+        if (certTypes != null && certTypes.size() > 0) {
+            // 电子证照数据
+            AeaCert scert = new AeaCert();
+            scert.setRootOrgId(rootOrgId);
+            List<AeaCert> certs = aeaCertMapper.listAeaCert(scert);
+            for (AeaCertType certType : certTypes) {
+                ZtreeNode certTypeNode = new ZtreeNode();
+                certTypeNode.setId(certType.getCertTypeId());
+                certTypeNode.setName(certType.getTypeName());
+                if (StringUtils.isBlank(certType.getParentTypeId())) {
+                    certTypeNode.setpId("root");
+                } else {
+                    certTypeNode.setpId(certType.getParentTypeId());
+                }
+                certTypeNode.setType("certType");
+                certTypeNode.setIsParent(true);
+                certTypeNode.setOpen(true);
+                certTypeNode.setNocheck(true);
+                allNodes.add(certTypeNode);
+                if (certs != null && certs.size() > 0) {
+                    List<AeaCert> removeCertList = new ArrayList<>();
+                    for (AeaCert cert : certs) {
+                        if (cert.getCertTypeId().equals(certType.getCertTypeId())) {
+                            ZtreeNode certNode = new ZtreeNode();
+                            certNode.setId(cert.getCertId());
+                            certNode.setName(cert.getCertName());
+                            certNode.setpId(certType.getCertTypeId());
+                            certNode.setType("cert");
+                            certNode.setIsParent(false);
+                            certNode.setOpen(true);
+                            certNode.setNocheck(false);
+                            allNodes.add(certNode);
+                            removeCertList.add(cert);
+                        }
+                    }
+                    // 移除已经遍历过的证照
+                    certs.removeAll(removeCertList);
+                }
+            }
+        }
+        return allNodes;
+    }
+
+    
+    public Long getMaxCertSortNo(String rootOrgId) {
+
+        if (StringUtils.isBlank(rootOrgId)) {
+            rootOrgId = SecurityContext.getCurrentOrgId();
+        }
+        Long sortNo = aeaCertMapper.getMaxCertSortNo(rootOrgId);
+        return sortNo == null ? 1L : sortNo + 1;
+    }
+
+    
+    public AeaCert getAeaCertById(String id) {
+
+        if (StringUtils.isBlank(id)) {
+            throw new InvalidParameterException(id);
+        }
+        logger.debug("根据ID获取Form对象，ID为：{}", id);
+        return aeaCertMapper.selectOneById(id);
+    }
+
+    
+    public PageInfo<AeaCert> listStageNoSelectCertByPage(AeaParIn aeaParIn, Page page) {
+
+        aeaParIn.setRootOrgId(SecurityContext.getCurrentOrgId());
+        PageHelper.startPage(page);
+        List<AeaCert> list = aeaCertMapper.listStageNoSelectCert(aeaParIn);
+        return new PageInfo<>(list);
+    }
+
+    
+    public List<AeaCert> listStageNoSelectCert(AeaParIn aeaParIn) {
+
+        aeaParIn.setRootOrgId(SecurityContext.getCurrentOrgId());
+        return aeaCertMapper.listStageNoSelectCert(aeaParIn);
+    }
+
+    
+    public PageInfo<AeaCert> listItemNoSelectCertByPage(AeaItemInout inout, Page page) {
+
+        inout.setRootOrgId(SecurityContext.getCurrentOrgId());
+        PageHelper.startPage(page);
+        List<AeaCert> list = aeaCertMapper.listItemNoSelectCert(inout);
+        return new PageInfo<>(list);
+    }
+
+    
+    public List<AeaCert> listItemNoSelectCert(AeaItemInout inout) {
+
+        inout.setRootOrgId(SecurityContext.getCurrentOrgId());
+        return aeaCertMapper.listItemNoSelectCert(inout);
+    }
+
+    
+    public List<AeaCert> getOutputCertsByIteminstId(String iteminstId) throws Exception {
+
+        if (StringUtils.isBlank(iteminstId)) return null;
+
+        AeaHiIteminst iteminst = aeaHiIteminstService.getAeaHiIteminstById(iteminstId);
+
+        if (iteminst == null) return null;
+
+        return aeaCertMapper.getOutputCertsByItemVerId(iteminst.getItemVerId(), SecurityContext.getCurrentOrgId());
+    }
+
+
+    
+    public LicenseAuthResDTO getLicenseAuthRes(String itemVerIds, String identityNumber) throws Exception {
+        String accessToken = licenseApiService.getLoginToken();
+        if (StringUtils.isBlank(itemVerIds) || StringUtils.isBlank(identityNumber))
+            return new LicenseAuthResDTO();
+        List<String> itemverIds = new ArrayList<>();
+        Collections.addAll(itemverIds, itemVerIds.split(","));
+        List<AeaItemBasic> list = aeaItemBasicMapper.getAeaItemBasicListByItemVerIds(itemverIds);
+        return getLicenseAuthRes(list, accessToken, identityNumber);
+    }
+
+    private LicenseAuthResDTO getLicenseAuthRes(List<AeaItemBasic> list, String accessToken, String identityNumber) {
+        if (list.size() < 1)
+            return new LicenseAuthResDTO();
+        LicenseAuthResDTO result = new LicenseAuthResDTO();
+        ArrayList checkAuthCodes = new ArrayList();//针对多个事项时，进行重复过滤
+        ArrayList auth_codes = new ArrayList();
+        List<LicenseDTO> licenseDTO = new ArrayList<>();
+        OpuOmUserInfo userinfo = opuOmUserInfoMapper.getOpuOmUserInfoByUserId(SecurityContext.getCurrentUserId());
+        OpuOmOrg topOrg = opuOmOrgService.getTopOrgByCurOrgId(SecurityContext.getCurrentOrgId());
+        BscDicRegion proDataRegion = bscDicRegionMapper.getBscDicRegionById(topOrg.getRegionId());
+        LicenseUserInfoDTO operator = new LicenseUserInfoDTO();
+        operator.setIdentity_num(userinfo.getUserIdCardNo() == null ? "null" : userinfo.getUserIdCardNo());//身份证号
+        operator.setDivision(topOrg.getOrgName());
+        operator.setDivision_code(proDataRegion.getRegionNum());
+        operator.setService_org(proDataRegion.getRegionNum());
+        operator.setService_org_code(topOrg.getOrgCode());
+        for (AeaItemBasic aeaItemBasic : list) {
+            LicenseAuthResDTO data = null;
+            LicenseAuthReqDTO authReqDTO = new LicenseAuthReqDTO();
+            authReqDTO.setService_item_name(aeaItemBasic.getItemName());
+            authReqDTO.setService_item_code(aeaItemBasic.getItemCode());//事项编码
+            authReqDTO.setIdentity_number(identityNumber);//证件号码（企业的信用代码/-/法人的身份证号）
+            authReqDTO.setOperator(operator);
+            try {
+                data = licenseApiService.licenseAuth(accessToken, authReqDTO);
+            } catch (Exception e) {
+                logger.debug("事项：" + aeaItemBasic.getItemName() + "查询电子证照库失败！");
+            }
+            if (data != null) {
+                for (int i = 0; i < data.getTotal_count(); i++) {
+                    if (checkAuthCodes.contains(data.getData().get(i).getLicense_code()))
+                        continue;
+                    checkAuthCodes.add(data.getData().get(i).getLicense_code());
+                    auth_codes.add(data.getAuth_codes()[i]);
+                    licenseDTO.add(data.getData().get(i));
+                }
+            }
+        }
+        if (licenseDTO.size() > 0) {
+            result.setAuth_codes((String[]) auth_codes.toArray(new String[auth_codes.size()]));
+            result.setData(licenseDTO);
+            result.setTotal_count((long) licenseDTO.size());
+        }
+        return result;
+    }
+
+    
+    public String getViewLicenseURL(String authCode) {
+        LicenseTokenResDTO LicenseTokenResDTO;
+        try {
+            String accessToken = licenseApiService.getLoginToken();
+            LicenseTokenResDTO = licenseApiService.LicenseToken(accessToken, authCode);
+            return licenseConfig.getWebRoot() + "/license/view_license?license_access_token=" + LicenseTokenResDTO.getData().getLicense_access_token();
+        } catch (Exception e) {
+            logger.debug("获取电子证照查看链接失败！");
+        }
+        return null;
+    }
+}
