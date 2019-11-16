45c90cbe3b41bd9bfbc5fdfd8e7b2202e7e9523b
wangermeng <2452503057@qq.com>
2019-11-16 14:28:23 +0800

    添加电子证照字段中文注释

--- aplanmis-common/src/main/java/com/augurit/aplanmis/integration/license/dto/LicenseAuthReqDTO.java
+++ aplanmis-common/src/main/java/com/augurit/aplanmis/integration/license/dto/LicenseAuthReqDTO.java
@@ -13,19 +13,19 @@ import java.io.Serializable;
 @Data
 public class LicenseAuthReqDTO implements Serializable {
 
-    private LicenseUserInfoDTO operator;
+    private LicenseUserInfoDTO operator;//发起此次用证的业务人员信息
 
-    private Long page_index;
+    private Long page_index;//返回第几页数据。默认值 1
 
-    private Long page_size;
+    private Long page_size;//返回数据的每页大小。默认值 10，最大值150
 
-    private String service_item_code;
+    private String service_item_code;//事项编码
 
-    private String service_item_name;
+    private String service_item_name;//事项名称
 
-    private String biz_num;
+    private String biz_num;//对应办件的业务流水号
 
-    private String identity_number;
+    private String identity_number;//证件号码。多个时用逗号（,）分隔
 
 
 }
