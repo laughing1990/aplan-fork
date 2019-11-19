4676541eb54357153a872617e464ead49f2b9866
wangch <agentwangz@163.com>
2019-11-19 15:36:26 +0800

    文件上传后缀判断漏洞处理

--- aplanmis-mall/src/main/java/com/augurit/aplanmis/mall/userCenter/service/RestFileService.java
+++ aplanmis-mall/src/main/java/com/augurit/aplanmis/mall/userCenter/service/RestFileService.java
@@ -1,6 +1,8 @@
 package com.augurit.aplanmis.mall.userCenter.service;
 
 import com.augurit.agcloud.framework.ui.result.ResultForm;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -26,5 +28,20 @@ public interface RestFileService {
      */
     Boolean isMatBelong(String matinstId,HttpServletRequest request)throws Exception;
 
+    /**
+     * 判断当前 detailId
+     * @param detailId
+     * @param request
+     * @return
+     * @throws Exception
+     */
     Boolean isFileBelong(String detailId,HttpServletRequest request)throws Exception;
+
+    /**
+     * 判断是否允许上传的文件类型
+     * @param request
+     * @return
+     */
+    Boolean isAllowFileType(HttpServletRequest request);
+
 }
