92406a553124489c1615190b012ff8ff580e9a6d
tinyXiong <xiongyb@augurit.com>
2019-11-20 10:13:29 +0800

    移动端企业用户登录改造 https设置

--- aplanmis-rest/src/main/java/com/augurit/aplanmis/rest/login/LoginController.java
+++ aplanmis-rest/src/main/java/com/augurit/aplanmis/rest/login/LoginController.java
@@ -5,11 +5,7 @@ import com.augurit.agcloud.framework.ui.result.ResultForm;
 import com.augurit.aplanmis.rest.auth.AuthUser;
 import com.augurit.aplanmis.rest.auth.JwtToken;
 import com.augurit.aplanmis.rest.common.service.CommonLoginService;
-import com.augurit.aplanmis.rest.common.vo.LoginInfoVo;
-import com.augurit.aplanmis.rest.login.jwx.AccessToken;
 import com.augurit.aplanmis.rest.login.jwx.JwtHelper;
-import com.augurit.aplanmis.rest.login.jwx.Jwx;
-import com.google.gson.Gson;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -24,10 +20,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,42 +29,11 @@ import java.util.Map;
 @RequestMapping("/rest/mall")
 @Api(value = "登录", tags = "登录 --> 相关接口")
 public class LoginController {
-    Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @Autowired
     private CommonLoginService commonLoginService;
-
-    Gson gson = new Gson();
-
-    @GetMapping("/login")
-    @ApiOperation(value = "登录")
-    @ApiImplicitParams({@ApiImplicitParam(value = "用户名", name = "userName", required = true, dataType = "string"),
-            @ApiImplicitParam(value = "密码(md5加密)", name = "password", required = true, dataType = "string")})
-    public ContentResultForm login(String userName, String password, HttpServletRequest request, HttpServletResponse response) {
-        try {
-            ContentResultForm login = commonLoginService.login(userName, password, "1", request);
-            if (login.isSuccess()) {
-                LoginInfoVo loginInfoVo = (LoginInfoVo) login.getContent();
-                Map<String, Object> map = new HashMap<String, Object>();
-                map.put("loginName", loginInfoVo.getLoginName());
-                String accessToken = JwtHelper.createJWT(gson.toJson(map));
-                AccessToken accessTokenEntity = new AccessToken();
-                accessTokenEntity.setAccessToken(accessToken);
-                accessTokenEntity.setExpiresIn(Jwx.expiration);
-                accessTokenEntity.setTokenType(Jwx.CLAIM_TOKEN_TYPE);
-                Cookie cookie = new Cookie(URLEncoder.encode(Jwx.CLAIM_KEY_NAME, "utf-8"),
-                        URLEncoder.encode(accessToken, "utf-8"));
-                cookie.setPath("/");
-                cookie.setHttpOnly(true);
-                response.addCookie(cookie);
-            }
-
-            return login;
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-            return new ContentResultForm<>(false, e.getMessage());
-        }
-    }
 
     @PostMapping("/mobile/login")
     @ApiOperation(value = "移动端登录")
