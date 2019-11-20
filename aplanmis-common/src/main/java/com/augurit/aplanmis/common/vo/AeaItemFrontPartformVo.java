50dcb1f16baf0c86d20521070fb658c772fdd670
xioahutu <1328669873@qq.com>
2019-11-20 09:36:28 +0800

    采购说明文件改为公共模块

--- aplanmis-common/src/main/java/com/augurit/aplanmis/common/vo/AeaItemFrontPartformVo.java
+++ aplanmis-common/src/main/java/com/augurit/aplanmis/common/vo/AeaItemFrontPartformVo.java
@@ -4,11 +4,13 @@ import com.augurit.aplanmis.common.domain.AeaItemFrontPartform;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author tiantian
  * @date 2019/11/11
  */
+@EqualsAndHashCode(callSuper = true)
 @Data
 @ApiModel(description = "事项前置扩展表检测Vo类")
 public class AeaItemFrontPartformVo extends AeaItemFrontPartform {
