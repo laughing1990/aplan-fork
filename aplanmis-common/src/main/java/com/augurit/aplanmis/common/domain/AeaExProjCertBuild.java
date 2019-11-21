08a9feee9cde5cc6a28beb86edde3e304d8a4f5c
LiaoJJ <359710655@qq.com>
2019-11-21 14:54:15 +0800

    时间参数

--- aplanmis-common/src/main/java/com/augurit/aplanmis/common/domain/AeaExProjCertBuild.java
+++ aplanmis-common/src/main/java/com/augurit/aplanmis/common/domain/AeaExProjCertBuild.java
@@ -69,7 +69,7 @@ public class AeaExProjCertBuild {
     private String creater;
     @ApiModelProperty("创建时间")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @JsonFormat(pattern = "yyyy-MM-dds")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date createTime;
     @ApiModelProperty("修改人")
     private String modifier;
