95b0735cd043abfa024246ecd64e14db78696e35
Lfuqiang <249425991@qq.com>
2019-11-26 14:04:10 +0800

    update： 中介超市=》 我的项目  模块页面处理

--- aplanmis-mall/src/main/resources/static/zjcs/personCenter/js/waitDealProDetail.js
+++ aplanmis-mall/src/main/resources/static/zjcs/personCenter/js/waitDealProDetail.js
@@ -3,7 +3,11 @@ var vm = new Vue({
     data: function () {
         return {
             activeNames: ['1', '2', '3'],
-            form: {},
+            form: {
+                aeaProjInfo: {},
+                iteminst: {},
+                purchaseProj: {}
+            },
             projScaleOptions: [{
                 value: '投资额（元）',
                 label: '投资额（元）'
@@ -25,15 +29,16 @@ var vm = new Vue({
     methods: {
         // 获取项目详情
         getDetail: function () {
+            var vm = this;
             var url = 'supermarket/purchase/getPurchaseDetail';
             var projPurchaseId = this.getUrlParam('id');
             request('', {
                 url: ctx + url,
                 type: 'get',
                 data: { projPurchaseId: projPurchaseId },
-            }, function (result) {
-                if (result.success) {
-
+            }, function (res) {
+                if (res.success) {
+                    vm.form = res.content;
                 }
 
             }, function (msg) { })
