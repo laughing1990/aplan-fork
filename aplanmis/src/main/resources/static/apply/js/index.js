8bb4a4304220220953d9262c2e773d4679d5e031
zoulingling <741786360@qq.com>
2019-11-26 17:20:24 +0800

    更新统一信用代码校验规则

--- aplanmis/src/main/resources/static/apply/js/index.js
+++ aplanmis/src/main/resources/static/apply/js/index.js
@@ -63,7 +63,7 @@ var vm = new Vue({
       if (value === '' || value === undefined || value.trim() === '') {
         callback(new Error('请输入统一社会信用代码！'));
       } else if (value) {
-        var flag = !/[0-9A-HJ-NPQRTUWXY]{2}\d{6}[0-9A-HJ-NPQRTUWXY]{10}/.test(value);
+        var flag = !/^[0-9A-HJ-NPQRTUWXY]{2}\d{6}[0-9A-HJ-NPQRTUWXY]{10}$/.test(value);
         if (flag) {
           return callback(new Error('请输入正确的统一社会信用代码！'));
         } else {
