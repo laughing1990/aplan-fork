46514881a209ce7f18ffe3d9491916217ef644e3
刘赵雄 <929404097@qq.com>
2019-11-27 14:54:07 +0800

    中介超市中介结构入驻审批接口

--- aplanmis/src/main/java/com/augurit/aplanmis/admin/market/register/controller/RegisterController.java
+++ aplanmis/src/main/java/com/augurit/aplanmis/admin/market/register/controller/RegisterController.java
@@ -1,20 +1,18 @@
 package com.augurit.aplanmis.admin.market.register.controller;
 
 
-import com.augurit.aplanmis.admin.market.register.service.RegisterService;
-
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
+
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 
+
 @RestController
 @RequestMapping("/supermarket/register")
 public class RegisterController {
-    @Autowired
-    RegisterService registerService;
 
     @RequestMapping("/index.html")
     @ApiOperation(value = "列表页面")
@@ -27,8 +25,6 @@ public class RegisterController {
     public ModelAndView registerDetail() {
         return new ModelAndView("admin/supermarket/register/register_detail");
     }
-
-
 
 
 }
