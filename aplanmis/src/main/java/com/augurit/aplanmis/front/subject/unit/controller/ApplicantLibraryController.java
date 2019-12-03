ae93386420d1797387b857dc24ae21a875ddf8cb
tinyXiong <xiongyb@augurit.com>
2019-12-03 11:00:11 +0800

    全局联系人 全局单位 新增编辑页面入口

--- aplanmis/src/main/java/com/augurit/aplanmis/front/subject/unit/controller/ApplicantLibraryController.java
+++ aplanmis/src/main/java/com/augurit/aplanmis/front/subject/unit/controller/ApplicantLibraryController.java
@@ -1,10 +1,15 @@
 package com.augurit.aplanmis.front.subject.unit.controller;
 
+import com.augurit.agcloud.framework.util.StringUtils;
+import com.augurit.aplanmis.common.domain.AeaUnitInfo;
+import com.augurit.aplanmis.common.mapper.AeaUnitInfoMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,10 +21,24 @@ public class ApplicantLibraryController {
 
     public static final String BACKEND_APPLICANT_GLOBAL_VIEW = "global/applicantview";//全局单位库
 
+    @Autowired
+    private AeaUnitInfoMapper aeaUnitInfoMapper;
+
     @GetMapping("/index")
     @ApiOperation("全局单位库-主页列表")
     public ModelAndView index() {
-//        return new ModelAndView("ui-jsp/global_view/global_applicant/global_applicant_view");
         return new ModelAndView(BACKEND_APPLICANT_GLOBAL_VIEW);
+    }
+
+    @GetMapping("/detail")
+    @ApiOperation("/企业单位详情")
+    public ModelAndView detail(@RequestParam(required = false) String unitInfoId) {
+        ModelAndView modelAndView = new ModelAndView("global/unitsDetail");
+        AeaUnitInfo aeaUnitInfo = null;
+        if (StringUtils.isNotBlank(unitInfoId)) {
+            aeaUnitInfo = aeaUnitInfoMapper.getAeaUnitInfoById(unitInfoId);
+        }
+        modelAndView.addObject("aeaUnitInfo", aeaUnitInfo);
+        return modelAndView;
     }
 }
