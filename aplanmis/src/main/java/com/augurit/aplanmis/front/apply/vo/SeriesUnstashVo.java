bb710c1305d088802e8579dc23a4f37dd3ad9235
tinyXiong <xiongyb@augurit.com>
2019-12-10 11:16:26 +0800

    回显接口

---
+++ aplanmis/src/main/java/com/augurit/aplanmis/front/apply/vo/SeriesUnstashVo.java
@@ -0,0 +1,33 @@
+package com.augurit.aplanmis.front.apply.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@ApiModel("单项回显vo")
+@Getter
+@Setter
+public class SeriesUnstashVo {
+
+    @ApiModelProperty(value = "项目id")
+    private String projInfoId;
+
+    @ApiModelProperty(value = "主题id")
+    private String themeId;
+
+    @ApiModelProperty(value = "主题版本id")
+    private String themeVerId;
+
+    @ApiModelProperty(value = "阶段id")
+    private String stageId;
+
+    @ApiModelProperty(value = "审批部门")
+    private String approveOrgId;
+
+    @ApiModelProperty(value = "情形id")
+    private Set<String> stateIds;
+
+}
