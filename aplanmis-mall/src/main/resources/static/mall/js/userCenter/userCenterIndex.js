79bb49c53f6e4a160c95b846b9e7d9aac891dc72
@Apple_QJP <285688338@qq.com>
2019-12-11 14:26:37 +0800

    网厅修复草稿箱数目展示问题

--- aplanmis-mall/src/main/resources/static/mall/js/userCenter/userCenterIndex.js
+++ aplanmis-mall/src/main/resources/static/mall/js/userCenter/userCenterIndex.js
@@ -125,7 +125,7 @@ var userCenter = (function () {
                 approvalNum: 0,
                 matCompletionNum: 0,
                 withdrawNum: 0,
-				        draftNum:0,
+                draftNum:0,
                 applyNum:0,
                 supplyNum:0,
                 withdrawalNum:0,
