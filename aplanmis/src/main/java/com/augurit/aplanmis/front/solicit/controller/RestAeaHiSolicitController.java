35bf32b6bef8501e3b79d9c057e2b90401cf4fd2
[chendx] <867759840@qq.com>
2019-12-23 14:15:11 +0800

    意见征求相关表代码逻辑
    (cherry picked from commit fdae7fa87ad71cabab7336c665b1a36e8fc7207b)

--- aplanmis/src/main/java/com/augurit/aplanmis/front/solicit/controller/RestAeaHiSolicitController.java
+++ aplanmis/src/main/java/com/augurit/aplanmis/front/solicit/controller/RestAeaHiSolicitController.java
@@ -1,9 +1,18 @@
 package com.augurit.aplanmis.front.solicit.controller;
 
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author:chendx
  * @date: 2019-12-23
  * @time: 13:47
  */
+@RestController
+@Slf4j
+@RequestMapping("/rest/solicit")
+@Api(tags = "意见征求入口")
 public class RestAeaHiSolicitController {
 }
--- aplanmis/src/main/java/com/augurit/aplanmis/front/solicit/service/impl/RestAeaHiSolicitServiceImpl.java
+++ aplanmis/src/main/java/com/augurit/aplanmis/front/solicit/service/impl/RestAeaHiSolicitServiceImpl.java
@@ -1,11 +1,15 @@
 package com.augurit.aplanmis.front.solicit.service.impl;
 
 import com.augurit.aplanmis.front.solicit.service.RestAeaHiSolicitService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author:chendx
  * @date: 2019-12-23
  * @time: 13:55
  */
+@Service
+@Transactional
 public class RestAeaHiSolicitServiceImpl implements RestAeaHiSolicitService{
 }
