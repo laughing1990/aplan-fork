5ecdaa7c3f3c67993956f90e1d827ff66b056ed7
fenggy <946864395@qq.com>
2019-12-23 11:06:17 +0800

    建设项目登记信息，建设项目两证一书信息，招投标信息增加fromId

--- aplanmis-common/src/main/java/com/augurit/aplanmis/common/domain/AeaExProjBid.java
+++ aplanmis-common/src/main/java/com/augurit/aplanmis/common/domain/AeaExProjBid.java
@@ -6,20 +6,20 @@ import org.springframework.format.annotation.DateTimeFormat;
 import java.io.Serializable;
 
 /**
-* 招投标信息-模型
-<ul>
-    <li>项目名：奥格erp3.0--第一期建设项目</li>
-    <li>版本信息：v1.0</li>
-    <li>日期：2019-10-31 15:56:12</li>
-    <li>版权所有(C)2016广州奥格智能科技有限公司-版权所有</li>
-    <li>创建人:Administrator</li>
-    <li>创建时间：2019-10-31 15:56:12</li>
-    <li>修改人1：</li>
-    <li>修改时间1：</li>
-    <li>修改内容1：</li>
-</ul>
-*/
-public class AeaExProjBid implements Serializable{
+ * 招投标信息-模型
+ * <ul>
+ * <li>项目名：奥格erp3.0--第一期建设项目</li>
+ * <li>版本信息：v1.0</li>
+ * <li>日期：2019-10-31 15:56:12</li>
+ * <li>版权所有(C)2016广州奥格智能科技有限公司-版权所有</li>
+ * <li>创建人:Administrator</li>
+ * <li>创建时间：2019-10-31 15:56:12</li>
+ * <li>修改人1：</li>
+ * <li>修改时间1：</li>
+ * <li>修改内容1：</li>
+ * </ul>
+ */
+public class AeaExProjBid implements Serializable {
 // ----------------------------------------------------- Properties
 
     private static final long serialVersionUID = 1L;
@@ -35,13 +35,13 @@ public class AeaExProjBid implements Serializable{
     private String bidSectionArea; // (表单面积，单位：平方米)
     private String bidSectionSpan; // (跨度，单位：米)
     private String bidSectionLength; // (长度，单位：里)
-    @DateTimeFormat(pattern="yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private java.util.Date winBidTime; // (中标日期)
     private String winBidMoney; // (实际中标金额，单位：万元)
     private String bidMemo; // (备注)
     private String creater; // (创建人)
-    @DateTimeFormat(pattern="yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private java.util.Date createTime; // (创建时间)
     private String modifier; // (修改人)
@@ -54,136 +54,185 @@ public class AeaExProjBid implements Serializable{
         this.tenderNum = tenderNum;
     }
 
-    @DateTimeFormat(pattern="yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
     private java.util.Date modifyTime; // (修改时间)
     private String rootOrgId; // (所属根组织ID)
 
-    private String  tenderNum;  //''省级中标通知书编号(广东三库一平台返回)',
 
-// ----------------------------------------------------- Constructors
+    //扩展字段
+    private String tenderNum;  //''省级中标通知书编号(广东三库一平台返回)',
+
+    private String formId;
+
+    // ----------------------------------------------------- Constructors
 // ----------------------------------------------------- Methods
-    public String getBidId(){
+
+    public String getFormId() {
+        return formId;
+    }
+
+    public void setFormId(String formId) {
+        this.formId = formId;
+    }
+
+    public String getBidId() {
         return bidId;
     }
-    public void setBidId( String bidId ) {
+
+    public void setBidId(String bidId) {
         this.bidId = bidId == null ? null : bidId.trim();
     }
-    public String getProjInfoId(){
+
+    public String getProjInfoId() {
         return projInfoId;
     }
-    public void setProjInfoId( String projInfoId ) {
+
+    public void setProjInfoId(String projInfoId) {
         this.projInfoId = projInfoId == null ? null : projInfoId.trim();
     }
-    public String getWinBidNoticeCode(){
+
+    public String getWinBidNoticeCode() {
         return winBidNoticeCode;
     }
-    public void setWinBidNoticeCode( String winBidNoticeCode ) {
+
+    public void setWinBidNoticeCode(String winBidNoticeCode) {
         this.winBidNoticeCode = winBidNoticeCode == null ? null : winBidNoticeCode.trim();
     }
-    public String getBidSectionName(){
+
+    public String getBidSectionName() {
         return bidSectionName;
     }
-    public void setBidSectionName( String bidSectionName ) {
+
+    public void setBidSectionName(String bidSectionName) {
         this.bidSectionName = bidSectionName == null ? null : bidSectionName.trim();
     }
-    public String getBidSectionAddr(){
+
+    public String getBidSectionAddr() {
         return bidSectionAddr;
     }
-    public void setBidSectionAddr( String bidSectionAddr ) {
+
+    public void setBidSectionAddr(String bidSectionAddr) {
         this.bidSectionAddr = bidSectionAddr == null ? null : bidSectionAddr.trim();
     }
-    public String getUnitAddr(){
+
+    public String getUnitAddr() {
         return unitAddr;
     }
-    public void setUnitAddr( String unitAddr ) {
+
+    public void setUnitAddr(String unitAddr) {
         this.unitAddr = unitAddr == null ? null : unitAddr.trim();
     }
-    public String getBidType(){
+
+    public String getBidType() {
         return bidType;
     }
-    public void setBidType( String bidType ) {
+
+    public void setBidType(String bidType) {
         this.bidType = bidType == null ? null : bidType.trim();
     }
-    public String getBidMode(){
+
+    public String getBidMode() {
         return bidMode;
     }
-    public void setBidMode( String bidMode ) {
+
+    public void setBidMode(String bidMode) {
         this.bidMode = bidMode == null ? null : bidMode.trim();
     }
-    public String getBidSectionMoney(){
+
+    public String getBidSectionMoney() {
         return bidSectionMoney;
     }
-    public void setBidSectionMoney( String bidSectionMoney ) {
+
+    public void setBidSectionMoney(String bidSectionMoney) {
         this.bidSectionMoney = bidSectionMoney == null ? null : bidSectionMoney.trim();
     }
-    public String getBidSectionArea(){
+
+    public String getBidSectionArea() {
         return bidSectionArea;
     }
-    public void setBidSectionArea( String bidSectionArea ) {
+
+    public void setBidSectionArea(String bidSectionArea) {
         this.bidSectionArea = bidSectionArea == null ? null : bidSectionArea.trim();
     }
-    public String getBidSectionSpan(){
+
+    public String getBidSectionSpan() {
         return bidSectionSpan;
     }
-    public void setBidSectionSpan( String bidSectionSpan ) {
+
+    public void setBidSectionSpan(String bidSectionSpan) {
         this.bidSectionSpan = bidSectionSpan == null ? null : bidSectionSpan.trim();
     }
-    public String getBidSectionLength(){
+
+    public String getBidSectionLength() {
         return bidSectionLength;
     }
-    public void setBidSectionLength( String bidSectionLength ) {
+
+    public void setBidSectionLength(String bidSectionLength) {
         this.bidSectionLength = bidSectionLength == null ? null : bidSectionLength.trim();
     }
+
     public java.util.Date getWinBidTime() {
         return winBidTime;
     }
-    public void setWinBidTime( java.util.Date winBidTime ){
+
+    public void setWinBidTime(java.util.Date winBidTime) {
         this.winBidTime = winBidTime;
     }
 
-    public String getWinBidMoney(){
+    public String getWinBidMoney() {
         return winBidMoney;
     }
-    public void setWinBidMoney( String winBidMoney ) {
+
+    public void setWinBidMoney(String winBidMoney) {
         this.winBidMoney = winBidMoney == null ? null : winBidMoney.trim();
     }
-    public String getBidMemo(){
+
+    public String getBidMemo() {
         return bidMemo;
     }
-    public void setBidMemo( String bidMemo ) {
+
+    public void setBidMemo(String bidMemo) {
         this.bidMemo = bidMemo == null ? null : bidMemo.trim();
     }
-    public String getCreater(){
+
+    public String getCreater() {
         return creater;
     }
-    public void setCreater( String creater ) {
+
+    public void setCreater(String creater) {
         this.creater = creater == null ? null : creater.trim();
     }
+
     public java.util.Date getCreateTime() {
         return createTime;
     }
-    public void setCreateTime( java.util.Date createTime ){
+
+    public void setCreateTime(java.util.Date createTime) {
         this.createTime = createTime;
     }
 
-    public String getModifier(){
+    public String getModifier() {
         return modifier;
     }
-    public void setModifier( String modifier ) {
+
+    public void setModifier(String modifier) {
         this.modifier = modifier == null ? null : modifier.trim();
     }
+
     public java.util.Date getModifyTime() {
         return modifyTime;
     }
-    public void setModifyTime( java.util.Date modifyTime ){
+
+    public void setModifyTime(java.util.Date modifyTime) {
         this.modifyTime = modifyTime;
     }
 
-    public String getRootOrgId(){
+    public String getRootOrgId() {
         return rootOrgId;
     }
-    public void setRootOrgId( String rootOrgId ) {
+
+    public void setRootOrgId(String rootOrgId) {
         this.rootOrgId = rootOrgId == null ? null : rootOrgId.trim();
     }
     //public String getTableName()  {
