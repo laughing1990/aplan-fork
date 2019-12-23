4844a19d4d8fd6bcc3c6e9011001af8bcba0ac97
wangch <agentwangz@163.com>
2019-12-23 16:08:36 +0800

    修改查看附件至法律法规列表

--- aplanmis-mall/src/main/java/com/augurit/aplanmis/mall/legal/service/RestLegalServiceImpl.java
+++ aplanmis-mall/src/main/java/com/augurit/aplanmis/mall/legal/service/RestLegalServiceImpl.java
@@ -1,14 +1,19 @@
 package com.augurit.aplanmis.mall.legal.service;
 
 
+import com.augurit.agcloud.bsc.domain.BscAttForm;
+import com.augurit.agcloud.bsc.sc.att.service.IBscAttService;
 import com.augurit.aplanmis.common.domain.AeaServiceLegal;
 import com.augurit.aplanmis.common.dto.ApproveProjInfoDto;
 import com.augurit.aplanmis.common.mapper.AeaServiceLegalMapper;
+import com.augurit.aplanmis.common.service.admin.legal.AeaServiceLegalAdminService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,12 +21,22 @@ public class RestLegalServiceImpl {
 
     @Autowired
     AeaServiceLegalMapper aeaServiceLegalMapper;
-
+    @Autowired
+    IBscAttService bscAttService;
+    @Autowired
+    AeaServiceLegalAdminService legalAdminService;
+    @Value("${dg.sso.access.platform.org.top-org-id:0368948a-1cdf-4bf8-a828-71d796ba89f6}")
+    protected String topOrgId;
     public PageInfo<AeaServiceLegal> getLegalListByKeyword(String keyword, int pageNum, int pageSize) throws Exception {
         AeaServiceLegal aeaServiceLegal = new AeaServiceLegal();
         aeaServiceLegal.setLegalLevel(keyword);
         PageHelper.startPage(pageNum,pageSize);
         List<AeaServiceLegal> list = aeaServiceLegalMapper.listAeaServiceLegal(aeaServiceLegal);
+        for (AeaServiceLegal legal:list){
+            List<BscAttForm> attList = bscAttService.listAttLinkAndDetailNoPage("AEA_SERVICE_LEGAL", "SERVICE_LEGAL_ATT", legal.getLegalId(), null, topOrgId, null);
+            legal.setLegalAtts((attList == null||attList.size()==0) ?new ArrayList<>(0):attList.subList(0,1));
+            legal.setServiceLegalAttCount(attList == null ? 0L : attList.size());
+        }
         return new PageInfo(list);
     }
 }
