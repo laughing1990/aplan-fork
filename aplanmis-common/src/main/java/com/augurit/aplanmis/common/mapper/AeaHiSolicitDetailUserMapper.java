15ce22efb5e5e40b40013b04c286b6d2e35f0cfa
xioahutu <1328669873@qq.com>
2019-12-24 14:14:08 +0800

    意见征求列表-todo

--- aplanmis-common/src/main/java/com/augurit/aplanmis/common/mapper/AeaHiSolicitDetailUserMapper.java
+++ aplanmis-common/src/main/java/com/augurit/aplanmis/common/mapper/AeaHiSolicitDetailUserMapper.java
@@ -3,21 +3,30 @@ package com.augurit.aplanmis.common.mapper;
 import com.augurit.aplanmis.common.domain.AeaHiSolicitDetailUser;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-    /**
-    * 征求意见详情用户任务表-Mapper数据与持久化接口类
-    */
-    @Mapper
-    public interface AeaHiSolicitDetailUserMapper {
+/**
+ * 征求意见详情用户任务表-Mapper数据与持久化接口类
+ */
+@Mapper
+@Repository
+public interface AeaHiSolicitDetailUserMapper {
 
     public void insertAeaHiSolicitDetailUser(AeaHiSolicitDetailUser aeaHiSolicitDetailUser) throws Exception;
+
     public void batchInsertAeaHiSolicitDetailUser(List<AeaHiSolicitDetailUser> aeaHiSolicitDetailUser) throws Exception;
+
     public void updateAeaHiSolicitDetailUser(AeaHiSolicitDetailUser aeaHiSolicitDetailUser) throws Exception;
+
     public void deleteAeaHiSolicitDetailUser(@Param("id") String id) throws Exception;
-    public List <AeaHiSolicitDetailUser> listAeaHiSolicitDetailUser(AeaHiSolicitDetailUser aeaHiSolicitDetailUser) throws Exception;
-    public List <AeaHiSolicitDetailUser> getAeaHiSolicitDetailUserBySolicitDetailId(@Param("solicitDetailId") String solicitDetailId) throws Exception;
-    public List <AeaHiSolicitDetailUser> getAeaHiSolicitDetailUserBySolicitId(@Param("solicitId") String solicitId) throws Exception;
+
+    public List<AeaHiSolicitDetailUser> listAeaHiSolicitDetailUser(AeaHiSolicitDetailUser aeaHiSolicitDetailUser) throws Exception;
+
+    public List<AeaHiSolicitDetailUser> getAeaHiSolicitDetailUserBySolicitDetailId(@Param("solicitDetailId") String solicitDetailId) throws Exception;
+
+    public List<AeaHiSolicitDetailUser> getAeaHiSolicitDetailUserBySolicitId(@Param("solicitId") String solicitId) throws Exception;
+
     public AeaHiSolicitDetailUser getAeaHiSolicitDetailUserById(@Param("id") String id) throws Exception;
-    }
+}
