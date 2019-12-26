a92615f1aafe8541dd59bbde04aa71e1b888e017
freddy <1358627726@qq.com>
2019-12-26 11:52:14 +0800

    【样式修改】佛山网厅4.1.0办事指南材料一单清

---
+++ aplanmis-mall/src/main/resources/static/mall/js/listmatter/listmatter.js
@@ -0,0 +1,38 @@
+var listmatter = (function(){
+    var vm = new Vue({
+        el:"#listmatter",
+        data:{
+            ctx:ctx,
+            tableData: [{
+                date: '2016-05-03',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-02',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-04',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-01',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-08',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-06',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-07',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }],
+            radio:'1',
+        }
+    })
+})()
