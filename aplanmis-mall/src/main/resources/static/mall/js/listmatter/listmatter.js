684d5ac04588ed718c37c49415aa38edb0f95cf4
freddy <1358627726@qq.com>
2019-12-30 16:25:30 +0800

    【功能开发】佛山网厅4.1.0办事指南头部搜索功能

--- aplanmis-mall/src/main/resources/static/mall/js/listmatter/listmatter.js
+++ aplanmis-mall/src/main/resources/static/mall/js/listmatter/listmatter.js
@@ -1,4 +1,4 @@
-var listmatter = (function(){
+var listmatter = (function(window){
     var vm = new Vue({
         el:"#listmatter",
         data:{
@@ -33,6 +33,29 @@ var listmatter = (function(){
                 address: '上海市普陀区金沙江路 1518 弄'
             }],
             radio:'1',
+        },
+        methods:{
+            getlistMatter:function (stageId) {
+                request('', {
+                    url: ctx + 'rest/guide/itemAndState/list/'+ stageId,
+                    type: 'get'
+                }, function (res) {
+
+                    if (res.success) {
+                        var content = res.content;
+
+                    } else {
+                        vm.$message.error(res.message);
+                    }
+
+                }, function () {
+                    vm.$message.error('获取事项一清单数据接口失败，请稍后重试！');
+                });
+            },
+            gotoGuideIndex:function () {
+                window.location.hash='/guideIndex';
+                window.location.search='';
+            }
         }
     })
-})()
+})(window);
