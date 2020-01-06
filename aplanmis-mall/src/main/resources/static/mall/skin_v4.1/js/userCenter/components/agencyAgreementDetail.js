2d49138d765aaaa11b63c742eed6cafb730f7eef
cxin <13580112575@163.com>
2020-01-06 17:14:13 +0800

    cx-网厅4.1代办保存数据1-6

--- aplanmis-mall/src/main/resources/static/mall/skin_v4.1/js/userCenter/components/agencyAgreementDetail.js
+++ aplanmis-mall/src/main/resources/static/mall/skin_v4.1/js/userCenter/components/agencyAgreementDetail.js
@@ -43,24 +43,122 @@ var module = new Vue({
       projectAddress: '',
       isAreaEstimate: '0',
       areaDetailCode: '', //建设地点详情
-      currentProgress: '',  //项目当前进度
-      agentStageState: [],   //具体委托阶段
+      currentProgress: '', //项目当前进度
+      agentStageState: [], //具体委托阶段
 
       // 项目单位信息
-      applicant: '',  //单位名称
-      applicantDetailSite: '',  //单位地址
-      email: '',  // 电子邮箱
-      unitNature: '',  //单位性质
-      leaderName: '',   //项目负责人
+      applicant: '', //单位名称
+      applicantDetailSite: '', //单位地址
+      email: '', // 电子邮箱
+      unitNature: '', //单位性质
+      leaderName: '', //项目负责人
       leaderMobilePhone: '', //联系方式
       leaderDuty: '', //负责人职务
       operatorName: '', //项目经办人
-      operatorMobilePhone: '',  //经办人联系方式
-      operatorDuty: ''   //经办人职务
+      operatorMobilePhone: '', //经办人联系方式
+      operatorDuty: '' //经办人职务
+    },
+
+    // 项目基本信息的-项目类型跟建设性质的下拉选项
+    proJAllOptions: {
+      XM_PROJECT_STEP: [], //项目类型
+      XM_NATURE: [], //建设性质
+      XM_PROJECT_LEVEL: [], //项目级别
+      XM_PROJECT_STEP: [], //立项类型
+      XM_TZLX: [], // 投资类型
+      XM_ZJLY: [], // 资金来源
+      XM_GBHY: [], // 行业类别
+      XM_TDLY: [], // 土地来源
+      XM_NATURE: [], // 建设性质
+      XM_GCFL: [] // 工程分类
     },
   },
-  methods: {},
-  created: function () {
+  methods: {
+    // 获取当前代办项目的信息
+    fetchProjInfo: function () {
+      var ts = this,
+        _url = ctx + 'rest/apply/agent/detail';
+      ts.dloading = true;
+      request('', {
+        url: _url,
+        type: 'get',
+        data: {
+          projInfoId: pager.curHandelProj.projInfoId,
+          applyAgentId: pager.curHandelProj.applyAgentId
+        }
+      }, function (res) {
+        ts.dloading = false;
+        if (res.success) {
+          ts.handelProjInfoData(res.content)
+        } else {
+          return ts.apiMessage(res.message, 'error')
+        }
+      }, function () {
+        ts.dloading = false;
+        return ts.apiMessage('网络错误！', 'error')
+      });
+    },
+    // 处理当先代办项目信息的数据回显
+    handelProjInfoData: function (data) {
+      var aeaUnitProjLinkmanVo = data.aeaUnitProjLinkmanVo,
+        projInfo = data.projInfo;
+      var formData = $.extend({}, projInfo, aeaUnitProjLinkmanVo);
+      for (var k in this.projInfoForm) {
+        if (formData[k]) {
+          this.projInfoForm[k] = formData[k];
+        }
+      }
+      this.projInfoForm.agentStageState = data.agentStageState;
+      this.dicFormat(this.projInfoForm.financialSource, 'XM_ZJLY', 'financialSourceCn');
+      this.dicFormat(this.projInfoForm.landSource, 'XM_TDLY', 'landSourceCn');
+    },
 
+    // 获取待选项
+    fetchDicContents: function () {
+      var ts = this;
+      ts.dloading = true;
+      request('', {
+        url: ctx + 'rest/user/getDicContents',
+        type: 'get',
+      }, function (res) {
+        ts.dloading = false;
+        if (res.success) {
+          ts.proJAllOptions = res.content;
+          ts.fetchProjInfo();
+        } else {
+          return ts.apiMessage(res.message, 'error')
+        }
+      }, function () {
+        ts.dloading = false;
+        return ts.apiMessage('网络错误！', 'error')
+      });
+    },
+
+    // 处理资金来源+土地性质
+    dicFormat: function (val, type, target) {
+      var ts = this,
+        str = '',
+        all = ts.proJAllOptions[type];
+      for (var i = 0, ln = all.length; i < ln; i++) {
+        if (val == all[i].itemCode) {
+          str = all[i].itemName;
+          break;
+        }
+      }
+      ts.projInfoForm[target] = str;
+    },
+  },
+  created: function () {
+    // ts.fetchProjInfo();
+  },
+  mounted: function () {
+    this.fetchDicContents();
+
+  },
+  filters: {
+    unitNatureFormat: function(val){
+      var cnList = ['企业','事业单位','社会组织'];
+      return cnList[val] || '-'
+    },
   },
 })
