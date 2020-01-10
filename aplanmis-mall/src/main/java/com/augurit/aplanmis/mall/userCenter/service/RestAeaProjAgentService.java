ab090e11e5e89917102f0bebf1a25d8e41f78ca5
@Apple_QJP <285688338@qq.com>
2020-01-10 12:28:43 +0800

    修复代办上传接口

--- aplanmis-mall/src/main/java/com/augurit/aplanmis/mall/userCenter/service/RestAeaProjAgentService.java
+++ aplanmis-mall/src/main/java/com/augurit/aplanmis/mall/userCenter/service/RestAeaProjAgentService.java
@@ -177,7 +177,7 @@ public class RestAeaProjAgentService {
             agentAgreementVo.setAgentUserName(applyAgent.getAgentUserName());
             agentAgreementVo.setAgentUserMobile(applyAgent.getAgentUserMobile());
             //List<BscAttForm> atts = bscAttMapper.listAttLinkAndDetailByTablePKRecordId("AEA_PROJ_APPLY_AGENT", "APPLY_AGENT_ID", applyAgentId, SecurityContext.getCurrentOrgId());
-            List<BscAttFileAndDir> atts = restFileService.getAttFilesByPK("AEA_PROJ_APPLY_AGENT", "APPLY_AGENT_ID", applyAgentId);
+            List<BscAttFileAndDir> atts = restFileService.getAttFilesByPK("AEA_PROJ_APPLY_AGENT", "AGREEMENT_CODE", applyAgent.getAgreementCode());
             agentAgreementVo.setAtts(atts);
             agentAgreementVo.setDetailId( atts.size()>0?atts.get(0).getBscAttDetail().getDetailId():null);
             list.add(agentAgreementVo);
