189baa46a73eea766400156bca2569b784b2ba0d
lucas <450964475@qq.com>
2020-01-15 18:46:31 +0800

    广东工建系统项目拆分子工程赋码

--- aplanmis-common/src/main/java/com/augurit/aplanmis/common/utils/HttpUtil.java
+++ aplanmis-common/src/main/java/com/augurit/aplanmis/common/utils/HttpUtil.java
@@ -12,13 +12,15 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
@@ -30,7 +32,6 @@ import java.util.Map;
 
 /**
  * @author GWCheng
- *
  */
 public class HttpUtil {
 
@@ -48,28 +49,30 @@ public class HttpUtil {
 
     /**
      * 发送HttpGet请求
+     *
      * @param url
      * @return 返回响应体
      */
     public static CloseableHttpResponse sendGet(String url) {
-        return sendGetRequest(url,true);
+        return sendGetRequest(url, true);
     }
 
     /**
      * 发送HttpGet请求
+     *
      * @param url
      * @param useDefaultUserAgent 请求头使用默认的User-Agent设置
      * @return 返回响应体
      */
-    public static CloseableHttpResponse sendGetRequest(String url,boolean useDefaultUserAgent) {
+    public static CloseableHttpResponse sendGetRequest(String url, boolean useDefaultUserAgent) {
 
-        if(url!=null) {
-            LOGGER.info("sendGetRequest 请求url:{},useDefaultUserAgent:{}", url,useDefaultUserAgent);
+        if (url != null) {
+            LOGGER.info("sendGetRequest 请求url:{},useDefaultUserAgent:{}", url, useDefaultUserAgent);
         }
 
         HttpGet httpget = new HttpGet(url);
 
-        if(useDefaultUserAgent) {
+        if (useDefaultUserAgent) {
             httpget.setHeader(USER_AGENT, DEFAULT_USER_AGENT_VALUE);
         }
         httpget.setHeader(CONTENT_TYPE, DEFAULT_CONTENT_TYPE_VALUE);
@@ -80,9 +83,9 @@ public class HttpUtil {
             response = httpclient.execute(httpget);
             return response;
         } catch (IOException e) {
-            LOGGER.error(e.getMessage(),e);
-        }finally {
-            if(response!=null){
+            LOGGER.error(e.getMessage(), e);
+        } finally {
+            if (response != null) {
                 try {
                     response.close();
                 } catch (IOException e) {
@@ -101,28 +104,29 @@ public class HttpUtil {
      * @param decodeBase64 返回是否需要base64解密
      * @return 返回json对象
      */
-    public static JSONObject sendGet(String url,boolean decodeBase64) {
+    public static JSONObject sendGet(String url, boolean decodeBase64) {
 
-        return sendGet(url,decodeBase64,true);
+        return sendGet(url, decodeBase64, true);
     }
 
     /**
      * 发送HttpGet请求
+     *
      * @param url
-     * @param decodeBase64 返回是否需要base64解密
+     * @param decodeBase64        返回是否需要base64解密
      * @param useDefaultUserAgent 请求头使用默认的User-Agent设置
      * @return 返回json对象
      */
-    public static JSONObject sendGet(String url,boolean decodeBase64,boolean useDefaultUserAgent) {
+    public static JSONObject sendGet(String url, boolean decodeBase64, boolean useDefaultUserAgent) {
 
-        if(url!=null) {
-            LOGGER.info("sendGet 请求url:{},decodeBase64:{},useDefaultUserAgent:{}", url,decodeBase64,useDefaultUserAgent);
+        if (url != null) {
+            LOGGER.info("sendGet 请求url:{},decodeBase64:{},useDefaultUserAgent:{}", url, decodeBase64, useDefaultUserAgent);
         }
 
         JSONObject result = null;
         HttpGet httpget = new HttpGet(url);
 
-        if(useDefaultUserAgent) {
+        if (useDefaultUserAgent) {
             httpget.setHeader(USER_AGENT, DEFAULT_USER_AGENT_VALUE);
         }
         httpget.setHeader(CONTENT_TYPE, DEFAULT_CONTENT_TYPE_VALUE);
@@ -137,19 +141,19 @@ public class HttpUtil {
             if (entity != null) {
                 String resultStr = EntityUtils.toString(entity, Consts.UTF_8);
 
-                LOGGER.info("请求返回:{}",resultStr);
+                LOGGER.info("请求返回:{}", resultStr);
 
-                if(decodeBase64){
+                if (decodeBase64) {
                     resultStr = new String(Base64.decodeBase64(resultStr.getBytes()));
 
-                    LOGGER.info("请求返回decodeBase64:{}",resultStr);
+                    LOGGER.info("请求返回decodeBase64:{}", resultStr);
                 }
-                result =  JSON.parseObject(resultStr);
+                result = JSON.parseObject(resultStr);
             }
         } catch (ParseException | IOException e) {
-            LOGGER.error(e.getMessage(),e);
+            LOGGER.error(e.getMessage(), e);
         } finally {
-            if(response!=null) {
+            if (response != null) {
                 try {
                     response.close();
                 } catch (IOException e) {
@@ -167,20 +171,20 @@ public class HttpUtil {
      * @return 返回结果对象
      */
     public static JSONObject sendPost(String url) {
-        return sendPost(url,null,false,false);
+        return sendPost(url, null, false, false);
     }
 
     /**
      * 发送HttpPost请求，参数为map
      *
      * @param url
-     * @param map 请求参数
+     * @param map          请求参数
      * @param decodeBase64 返回是否需要base64解密
      * @return 返回结果对象
      */
-    public static JSONObject sendPost(String url, Map<String, String> map,String decodeBase64) {
+    public static JSONObject sendPost(String url, Map<String, String> map, String decodeBase64) {
 
-        return sendPost(url,map,"true".equals(decodeBase64)?true:false,true);
+        return sendPost(url, map, "true".equals(decodeBase64) ? true : false, true);
 
     }
 
@@ -188,15 +192,15 @@ public class HttpUtil {
      * 发送HttpPost请求，参数为map
      *
      * @param url
-     * @param map 请求参数
-     * @param decodeBase64 返回是否需要base64解密
+     * @param map                 请求参数
+     * @param decodeBase64        返回是否需要base64解密
      * @param useDefaultUserAgent 请求头使用默认的User-Agent设置
      * @return
      */
-    public static JSONObject sendPost(String url, Map<String, String> map,boolean decodeBase64,boolean useDefaultUserAgent) {
+    public static JSONObject sendPost(String url, Map<String, String> map, boolean decodeBase64, boolean useDefaultUserAgent) {
 
-        if(url!=null) {
-            LOGGER.info("sendPost 请求url:{},请求参数:{},decodeBase64:{},useDefaultUserAgent:{}", url,map==null?"":map.toString(),decodeBase64,useDefaultUserAgent);
+        if (url != null) {
+            LOGGER.info("sendPost 请求url:{},请求参数:{},decodeBase64:{},useDefaultUserAgent:{}", url, map == null ? "" : map.toString(), decodeBase64, useDefaultUserAgent);
         }
 
         JSONObject result = null;
@@ -205,13 +209,13 @@ public class HttpUtil {
 
         HttpPost httppost = new HttpPost(url);
 
-        if(useDefaultUserAgent) {
+        if (useDefaultUserAgent) {
             httppost.setHeader(USER_AGENT, DEFAULT_USER_AGENT_VALUE);
         }
 
         httppost.setHeader(CONTENT_TYPE, DEFAULT_CONTENT_TYPE_VALUE);
 
-        if(map!=null) {
+        if (map != null) {
 
             List<NameValuePair> formparams = new ArrayList<>();
 
@@ -230,19 +234,19 @@ public class HttpUtil {
             if (httpEntity != null) {
                 String resultStr = EntityUtils.toString(httpEntity, Consts.UTF_8);
 
-                LOGGER.info("请求返回:{}",resultStr);
+                LOGGER.info("请求返回:{}", resultStr);
 
-                if(decodeBase64){
-                    resultStr = new String(Base64.decodeBase64(resultStr.getBytes()),Consts.UTF_8);
+                if (decodeBase64) {
+                    resultStr = new String(Base64.decodeBase64(resultStr.getBytes()), Consts.UTF_8);
 
-                    LOGGER.info("请求返回decodeBase64:{}",resultStr);
+                    LOGGER.info("请求返回decodeBase64:{}", resultStr);
                 }
-                result =  JSON.parseObject(resultStr);
+                result = JSON.parseObject(resultStr);
             }
         } catch (ParseException | IOException e) {
-            LOGGER.error(e.getMessage(),e);
-        }finally {
-            if(response!=null) {
+            LOGGER.error(e.getMessage(), e);
+        } finally {
+            if (response != null) {
                 try {
                     response.close();
                 } catch (IOException e) {
@@ -254,13 +258,14 @@ public class HttpUtil {
 
     /**
      * json请求
+     *
      * @param url
      * @param json
      * @return
      */
-    public static JSONObject doPostByJson(String url,JSONObject json){
+    public static JSONObject doPostByJson(String url, JSONObject json) {
 
-        LOGGER.info("doPostByJson 请求url:{},json:{}",url,json.toJSONString());
+        LOGGER.info("doPostByJson 请求url:{},json:{}", url, json.toJSONString());
 
         DefaultHttpClient client = new DefaultHttpClient();
 
@@ -274,17 +279,17 @@ public class HttpUtil {
             post.setEntity(stringEntity);
 
             res = client.execute(post);
-            if(res.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+            if (res.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
                 String result = EntityUtils.toString(res.getEntity());
 
-                LOGGER.info("请求返回{}",result);
+                LOGGER.info("请求返回{}", result);
 
                 response = JSON.parseObject(result);
             }
         } catch (Exception e) {
-            LOGGER.error(e.getMessage(),e);
-        }finally {
-            if(res!=null){
+            LOGGER.error(e.getMessage(), e);
+        } finally {
+            if (res != null) {
                 try {
                     res.close();
                 } catch (IOException e) {
@@ -297,12 +302,13 @@ public class HttpUtil {
 
     /**
      * POST请求 ,尝试请求3次
-     * @param postUrl       请求地址
-     * @param postHeaders   请求头
-     * @param postEntity    请求发送实体
+     *
+     * @param postUrl     请求地址
+     * @param postHeaders 请求头
+     * @param postEntity  请求发送实体
      * @return 提取HTTP响应报文包体，以字符串形式返回
      */
-    public static String httpPost(String postUrl, Map<String, String> postHeaders, String postEntity){
+    public static String httpPost(String postUrl, Map<String, String> postHeaders, String postEntity) {
         String resJson = "";
         HttpURLConnection httpURLConnection = null;
         try {
@@ -320,18 +326,18 @@ public class HttpUtil {
                 httpURLConnection.setInstanceFollowRedirects(true);
                 httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
                 StringBuilder sbStr = new StringBuilder();
-                if(postHeaders != null) {
-                    for(String pKey : postHeaders.keySet()) {
+                if (postHeaders != null) {
+                    for (String pKey : postHeaders.keySet()) {
                         httpURLConnection.setRequestProperty(pKey, postHeaders.get(pKey));
                     }
                 }
-                if(postEntity != null) {
+                if (postEntity != null) {
                     OutputStream outputStream = httpURLConnection.getOutputStream();
                     outputStream.write(new String(postEntity).getBytes("utf-8"));
                     outputStream.flush();
                     outputStream.close();
                     result = httpURLConnection.getResponseCode();
-                    if(result == 200){
+                    if (result == 200) {
                         BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
                         String inputLine;
                         while ((inputLine = in.readLine()) != null) {
@@ -341,7 +347,7 @@ public class HttpUtil {
                         // 处理响应结果
                         sendOk = true;
                         resJson = new String(sbStr.toString().getBytes(), "utf-8");
-                    }else{
+                    } else {
                         resJson = "远程服务器连接失败,错误代码:" + result;
                         System.out.println(resJson);
                         resendCount++;
@@ -357,5 +363,50 @@ public class HttpUtil {
             }
         }
         return resJson;
+    }
+
+    /**
+     * post请求，参数为json格式
+     *
+     * @param url
+     * @param jsonObject
+     * @return
+     * @throws ParseException
+     * @throws IOException
+     */
+    public static JSONObject sendPostJson(String url, JSONObject jsonObject) throws ParseException, IOException {
+        JSONObject responseJSON = null;
+        //创建httpclient对象
+        CloseableHttpClient client = HttpClients.createDefault();
+        //创建post方式请求对象
+        HttpPost httpPost = new HttpPost(url);
+        CloseableHttpResponse response = null;
+        try {
+            //装填参数
+            StringEntity s = new StringEntity(jsonObject.toString(), "utf-8");
+            s.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+            //设置参数到请求对象中
+            httpPost.setEntity(s);
+            System.out.println("请求地址：" + url);
+            //设置header信息
+            //指定报文头【Content-type】、【User-Agent】
+            //httpPost.setHeader("Content-type", "application/x-www-form-urlencoded");
+            httpPost.setHeader("Content-type", "application/json");
+            httpPost.setHeader("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+            //执行请求操作，并拿到结果（同步阻塞）
+            response = client.execute(httpPost);
+            //获取结果实体
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                //按指定编码转换结果实体
+                responseJSON = JSON.parseObject(EntityUtils.toString(entity, "UTF-8"));
+            }
+            EntityUtils.consume(entity);
+            return responseJSON;
+        } catch (Exception e) {
+            return null;
+        } finally {
+            response.close();  //释放链接
+        }
     }
 }
---
+++ aplanmis-common/src/main/java/com/augurit/aplanmis/thirdPlatform/service/ProjectSplitService.java
@@ -0,0 +1,7 @@
+package com.augurit.aplanmis.thirdPlatform.service;
+
+public interface ProjectSplitService {
+
+
+    String getGCBM(String applySplitId) throws Exception;
+}
