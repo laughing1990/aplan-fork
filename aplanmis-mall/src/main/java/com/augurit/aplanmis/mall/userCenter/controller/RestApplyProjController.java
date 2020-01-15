14083f6dde2a3204edf3551ad9fd5b43a665ead2
wangch <agentwangz@163.com>
2020-01-15 17:04:47 +0800

    fix:修复《查询监管平台下的项目信息》关键词空格搜索失败问题

--- aplanmis-mall/src/main/java/com/augurit/aplanmis/mall/userCenter/controller/RestApplyProjController.java
+++ aplanmis-mall/src/main/java/com/augurit/aplanmis/mall/userCenter/controller/RestApplyProjController.java
@@ -361,6 +361,7 @@ public class RestApplyProjController {
     @ApiImplicitParams({@ApiImplicitParam(value = "搜索关键字",name = "keyWord",required = false,dataType = "string")})
     public ContentResultForm<AeaProjInfo> getProjInfoFromThirdPlatform(HttpServletRequest request, @PathVariable("keyWord") String keyWord) {
         try {
+            if (StringUtils.isNotBlank(keyWord)) keyWord = keyWord.trim();
             List<AeaProjInfo> list = aeaProjInfoService.findAeaProjInfoByKeyword(keyWord);
             if (list.size()==0&&!keyWord.contains("#") && !keyWord.contains("ZBM") && CommonTools.isComplianceWithRules(keyWord)) {
                 list = projectCodeService.getProjInfoFromThirdPlatform(keyWord, "","");
