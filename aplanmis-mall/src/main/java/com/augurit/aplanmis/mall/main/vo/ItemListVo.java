77c8727840887b279b055d27f0b9b8614140cc44
wangch <agentwangz@163.com>
2020-01-17 09:15:06 +0800

    fix:智能引导根据阶段获取事项列表接口增加stageId,themeVerId,themeId,detailId等字段

--- aplanmis-mall/src/main/java/com/augurit/aplanmis/mall/main/vo/ItemListVo.java
+++ aplanmis-mall/src/main/java/com/augurit/aplanmis/mall/main/vo/ItemListVo.java
@@ -21,8 +21,16 @@ public class ItemListVo {
     List<AeaParState> stateList;
     @ApiModelProperty(value = "主题名称")
     String themeName;
+    @ApiModelProperty(value = "主题ID")
+    String themeId;
+    @ApiModelProperty(value = "主题版本ID")
+    String themeVerId;
     @ApiModelProperty(value = "阶段名称")
     String stageName;
+    @ApiModelProperty(value = "阶段ID")
+    String stageId;
+    @ApiModelProperty(value = "流程图ID")
+    String detailId;
     @ApiModelProperty(value = "工作日")
     Double dueNum;
     @ApiModelProperty("这个字段为1，说明阶段配了情形和材料，为0，说明就直接用事项的")
